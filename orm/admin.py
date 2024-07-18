d90972484eb69f3082d461f499ef19203f58a4a6
Eldorbek34 <tpgcom1@gmail.com>
2024-07-18 12:13:36 +0500

    17_iyul_django

--- orm/admin.py
+++ orm/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from orm.models import Region, Student
+from .models import Student, Region
 
-# Register your models here.
-admin.site.register(Region)
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'region', 'joined_at')
+    list_filter = ('region', 'joined_at')
+    search_fields = ('first_name', 'last_name', 'phone')
+    ordering = ('-joined_at',)
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
